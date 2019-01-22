21fccc22845807b4ba257da62d8299e5afded517
bonillamiranda3 <bonillamiranda3@gmail.com>
2019-01-22 19:22:00 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,17 @@
 #write your code here
 
-def countdown
-  x = 10
-    while x > 10
-      puts "#{x} seconds"
+def countdown(seconds)
+      while seconds > 0
+      puts "#{seconds} SECOND(S)!"
       x -= 1
       puts "Happy New Year!"
     end
+
+    def countdown_with_sleep
+      while seconds > 0
+        puts "#{seconds} SECOND(S)!"
+        seconds -= 1
+        sleep(1)
+end
+  "Happy New Year!"
 end
