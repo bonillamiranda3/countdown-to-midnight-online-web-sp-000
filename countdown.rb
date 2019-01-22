74dddc56b073d3292c76deb8cf7340d6dd8cc44d
bonillamiranda3 <bonillamiranda3@gmail.com>
2019-01-22 19:22:26 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,8 +4,10 @@ def countdown(seconds)
       while seconds > 0
       puts "#{seconds} SECOND(S)!"
       x -= 1
-      puts "Happy New Year!"
     end
+   "Happy New Year!"
+ end
+    
 
     def countdown_with_sleep
       while seconds > 0
